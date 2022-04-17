0ffd272682462ca3684d5141a617915248ce39bc
André Sanches <andre.sanches.1.12@gmail.com>
2022-04-16 21:49:05 -0400

    add example

--- modinfo.lua
+++ modinfo.lua
@@ -1,15 +1,11 @@
 name = "Story Books"
-description = ""
+description = "Read some books and increase your sanity! Others around you are also affected by a sanity aura."
 author = "andre"
 version = "1.0.0"
-
 icon_atlas = ""
 icon = ""
-
--- compatibilidade
 dont_starve_compatible = false
 dst_compatible = true
 client_only_mod = false
 all_clients_require_mod = true
-
 api_version = 10
